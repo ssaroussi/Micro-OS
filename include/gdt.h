62f484dca967c10416a13f4df0ba048f42ae0df8
Ford <amit_s@tutanota.com>
2017-08-07 15:31:22 +0300

    oops

--- include/gdt.h
+++ include/gdt.h
@@ -1,16 +1,18 @@
 #ifndef INCLUDE_GDT_H
 #define INCLUDE_GDT_H
 
-#define GDT_NUM_ENTRIES (6)
+#define GDT_NUM_ENTRIES (3)
+#define CODE_SEG_SIZE   (0xFFFFFFFF)
+#define DATA_SEG_SIZE   (0xFFFFFFFF)
 
 struct gdt_entry
 {
   unsigned short limitLow;
   unsigned short baseLow;
-  unsigned char baseMid;
-  unsigned char access;
-  unsigned char gran;
-  unsigned char baseHigh;
+  unsigned char  baseMid;
+  unsigned char  access;
+  unsigned char  gran;
+  unsigned char  baseHigh;
 } __attribute__((packed));
 
 typedef struct gdt_entry gdt_entry_t;
@@ -18,12 +20,16 @@ typedef struct gdt_entry gdt_entry_t;
 struct gdt_ptr
 {
   unsigned short limit;
-  unsigned int base;
+  unsigned int   base;
 } __attribute__((packed));
 
 typedef struct gdt_ptr gdt_ptr_t;
 
 
-void init_gdt(unsigned int addr);
+void init_gdt();
+extern void gdt_flush(); // at loader.s
+void gdt_set_gate(int index, unsigned long base, unsigned long limit,
+		  unsigned char access, unsigned char gran);
+
 
 #endif // INCLUDE_GDT_H
